dc2b0b07a1be749b25e77c79252c4b8168b665ab
Neil Hammerle <neilhammerle@Neils-MacBook-Pro.local>
2020-08-09 17:46:59 -0700

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Home from "./components/Home.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Home
   }
-}
+};
 </script>
 
 <style>
@@ -21,8 +19,15 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* text-align: center; */
+  color: white;
+  /* margin-top: 60px; */
+  padding: 0;
+  margin: 0;
+}
+body {
+  background: #080808;
+  padding: 0;
+  margin: 0;
 }
 </style>
