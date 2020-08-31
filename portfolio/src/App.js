7decb926f5d053c67234e9d5abffeccafcd615dd
Neil Hammerle <neilhammerle@Neils-MacBook-Pro.local>
2020-08-30 18:34:37 -0700

    adds updates on about page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -39,7 +39,7 @@ class App extends React.Component {
         <Container className="p-0" fluid={true}>
 
           <Navbar className='border-bottom' bg='transparent' expand='lg'>
-            <Navbar.Brand>Neil Hammerle</Navbar.Brand>
+            <Navbar.Brand href="/">Neil Hammerle</Navbar.Brand>
 
             <Navbar.Toggle className='border-0' aria-controls='navbar-toggle' />
             <Navbar.Collapse id='navbar-toggle'>
--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
+import Hero from '../components/Hero'
+import Content from '../components/Content'
+
 function About(props) {
     return (
-        <h1>Hello world, this about!</h1>
+        <div>
+            <Hero title={props.title} />
+
+            <Content>
+                <p>Hello, my name is Neil Hammerle. I'm full stack web developer with experince in React, Vue, Node.js, MySQL, and NoSQL</p>
+                <p>I hope one day I am able to start my own company and build something that could change the world</p>
+                <p>There are many things I would like to learn and build. I like to consider myself as a learner and always welling to learn something new!</p>
+                <p>The two projects that I'm currently working on are Chadder and Veganish. To learn more about them please click here or visit the home page!</p>
+                <p>When I'm not working on my projects, what I like to do on my free time is going to aventures. I like going on long hikes and bike rides.</p>
+                <p>Feel free to follow my aventures on my social media accounts below!</p>
+
+            </Content>
+        </div>
     );
 }
 
