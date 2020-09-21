271bef41386efd07c5f3948479473d126408bfdd
Neil Hammerle <neilhammerle@gmail.com>
2020-09-20 21:50:33 -0700

    Contact page updates

---
+++ portfolio/build 2/precache-manifest.1305c3cecb2ef0f40c356a910020ba32.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef005b14a30e63376b09ac64ec88ec1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bbedeb46ff8740b0cec",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "b59c1d355964bf00e383",
+    "url": "/static/css/main.5aeb5f86.chunk.css"
+  },
+  {
+    "revision": "9bbedeb46ff8740b0cec",
+    "url": "/static/js/2.8c49d3c2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8c49d3c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b59c1d355964bf00e383",
+    "url": "/static/js/main.1897d91d.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "a8d299584eaaf505083fc0802181bf04",
+    "url": "/static/media/chadder.a8d29958.png"
+  },
+  {
+    "revision": "477879b3fe0fc0ead5a47ed773b993a7",
+    "url": "/static/media/veganish.477879b3.png"
+  }
+]);
--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -1,9 +1,36 @@
 import React from 'react'
 
+import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/Row'
+
 const Contact = () => {
     return (
         <div>
-            <h1>Hello world!</h1>
+            <Container>
+                <Container>
+
+                </Container>
+            </Container>
+
+            <Container>
+                <Container>
+                    <Card>
+                        <h4>Email me: neilhammerle@gmail.com</h4>
+                        <h5>or follow me any of my social media accounts!</h5>
+
+                        <Row>
+                            <h5 className='col'>
+                                <a className="dark-text" href="https://github.com/neilhammerle">
+                                    <i className="fa fa-github">
+
+                                    </i>
+                                </a>
+                            </h5>
+                        </Row>
+                    </Card>
+                </Container>
+            </Container>
         </div>
     )
 }
